85e144333b241b73b02b4a99268e9dfea4748cba
Jacob Clark <jacob.clark@bbc.co.uk>
2019-02-10 17:32:56 +0000

    syntax error

--- simple-tcp-server.lisp
+++ simple-tcp-server.lisp
@@ -5,7 +5,7 @@
 ; Define our address to be 0.0.0.0 (public interface)
 (defparameter *address* '(0 0 0 0))
 ; Define our port to be 8080
-(defparameter *port* 8081)
+(defparameter *port* 8080)
 ; Connections to hold on the backlog
 (defparameter *socket-backlog* 100)
 ; Response to emit from the server
@@ -17,6 +17,10 @@
 ; Buffer to recieve data sent from the client as an octet stream
 (defparameter *receive-buffer* (make-array *receive-buffer-lenth* :element-type '(unsigned-byte 8) :initial-element 0 :adjustable :fill-pointer))
 
+(defun force-print-buffer(data)
+  (format t data)
+  (force-output t))
+
 (defun read-into-buffer(buffer socket bytes-left)
   ; Dynamically sized buffer
   (adjust-array buffer (list (1+ (length buffer))) :initial-element 0)
@@ -25,10 +29,6 @@
     ((= bytes-left 0) (force-print-buffer (octets-to-string buffer)))
     ; There are bytes left, recurse until there are not
     (t (read-into-buffer buffer socket (nth-value 1 (sb-bsd-sockets:socket-receive socket buffer (length buffer)))))))
-
-(defun force-print-buffer(data)
-  (format t data)
-  (force-output t))
  
 ; Bind our inet-socket to 0.0.0.0:8080
 (sb-bsd-sockets:socket-bind *socket* *address* *port*)
